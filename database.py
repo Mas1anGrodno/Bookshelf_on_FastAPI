cd8b39dccf46bf4707b2049c63c0d5580d92790d
”Kryhin <kryhin.amzn@gmail.com>
2024-11-22 10:54:41 +0300

    learning how to write basics

---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+db_engine = create_async_engine("sqlite+aiosqlite:///Bookshelf.db")  # адрес к базе
+
+new_session = async_sessionmaker(db_engine, expire_on_commit=False)
+
+
+class Model(DeclarativeBase):
+    pass
+
+
+class BookShelfOrm(Model):
+    __tablename__ = "Bookshelf"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    author: Mapped[str]
+    description: Mapped[str | None]  # str | None  == Optional [str]
+
+
+async def create_tables():
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+async def delete_tables():
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Model.metadata.drop_all)
--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+
+from database import create_tables, delete_tables
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("Дропаем базу базу")
+    await create_tables()
+    print("Стартуем базу")
+    yield
+    print("Тушим базу")
 
 
-@app.get("/home")
-def get_homepage():
-    return "fastapi is now working"
+app = FastAPI(lifespan=lifespan)
+
+
+class BooksGet(BaseModel):
+    name: str
+    author: str
+    description: str | None
+
+
+class BookAdd(BooksGet):
+    id: int
+
+
+bookShelf = []
+
+
+@app.post("/books")
+async def add_book(book: Annotated[BookAdd, Depends()]):
+    bookShelf.append(book)
+    return {"book added": True}
+
+
+# @app.get("/books")
+# def get_book():
+#     book1 = BooksGet(name="123", author="456", description="hfhfhfhfhhf")
+#     return {"data": book1}
